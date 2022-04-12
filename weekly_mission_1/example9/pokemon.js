83f9f3e080f3a6cea93b8f1b895afa10a6fa84fc
AldoNValen <launchx06915@innovaccion.mx>
2022-04-11 20:06:48 -0500

    practica 9

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,19 @@
+class pokemon{
+    constructor (name) {
+        this.name = name
+      }
+  
+      sayHello () {      
+
+        console.log('Mi pokemon ' + this.name + ' te saluda!')
+      }
+
+      sayMessage (message) {
+        this.count++
+        console.log('Mi pokemon ' + this.name + 'dice: ' + message)
+      }
+
+}
+
+
+module.exports= pokemon
