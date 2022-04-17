0f6b7f228f2e940b48faba3b83e39b83dd2f9751
AldoNValen <launchx06915@innovaccion.mx>
2022-04-17 00:49:41 -0500

    agregando ejercicio con objetos

--- exercises/Ejercicio2/lista.js
+++ exercises/Ejercicio2/lista.js
@@ -61,5 +61,32 @@ const explorers = [
    
    ]
 
-   explorers.forEach(name => console.log(explorers["name"]))
-  // explorers.forEach(name => console.log(name))
+console.log("Imprime el nombre (propiedad name) de cada explorer en la lista, usa FOR EACH")
+explorers.forEach(explorer => console.log(explorer.name))
+
+console.log("Imprime el stack de cada explorer, usa FOR EACH")
+explorers.forEach(item => console.log(item.stack))
+
+console.log("Crea una nueva lista con las listas de stacks de cada explorer, usa MAP")
+const lstExplorar_stacks= explorers.map(lista => lista.stack)
+console.log(lstExplorar_stacks)
+
+console.log("Obtén la lista de explorers que tengan en su stack js, usa FILTER (para validar un elemento en un lista se usa el método includes)")
+const explorers_con_js = explorers.filter(lista => lista.stack.includes("js"))
+console.log(explorers_con_js)
+
+console.log("Busca el primer explorer que sea de la CDMX, usa FIND")
+const explorar_cdmx = explorers.find(cdmx => cdmx.city== "CDMX")
+console.log(explorar_cdmx)
+
+console.log("Obtén la suma de todos los exercises_completed, usa REDUCE")
+const suma= explorers.reduce((acc,lista) => acc + lista.exercises_completed,0)
+console.log(suma)
+ 
+console.log("Obtén la validación si al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true, usa SOME")
+const verificar = explorers.some((validar) =>  validar.missions.frontend.exercisesFinished === true)
+console.log(verificar)
+ 
+console.log("Obtén la validación si todos los explorers tienen la propiedad isFinished del onboarding como true. Usa EVERY.")
+const todos = explorers.every((validarTodo) =>  validarTodo.missions.onboarding.isFinished === true)
+console.log(todos)
