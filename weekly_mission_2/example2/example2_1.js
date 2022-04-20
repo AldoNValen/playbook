6bda6bd899902340337547193e6e1f6cc08e3c51
AldoNValen <launchx06915@innovaccion.mx>
2022-04-19 22:57:31 -0500

    instanciando objetos

--- weekly_mission_2/example2/example2_1.js
+++ weekly_mission_2/example2/example2_1.js
@@ -1,7 +1,7 @@
 console.log("POO")
 
 //objeto de js
-const woopa={
+const woopa_obj={
     name:"Woopa",
     mission: "Node JS",
     age:"12",
@@ -10,7 +10,30 @@ const woopa={
 
 //crear una clase
 class Ajolonauta{
+    constructor(name,mission,age,color){
+    this.name=name
+    this.mission=mission
+    this.age=age
+    this.color=color
+        }
 
 }
 
 console.log(Ajolonauta)
+
+/* const woopa=new Ajolonauta() // instanciar un objeto
+const wooper=new Ajolonauta() // instanciar un objeto
+const woopy=new Ajolonauta() // instanciar un objeto , objeto instanciado de una clase */
+const woopa_obj_js={} //objeto de java script 
+console.log(woopa_obj_js)
+
+/* console.log(woopa)
+console.log(wooper)
+console.log(woopy)
+console.log(woopa_obj_js) */
+
+const woopa=new Ajolonauta("Woopa","Noe JS",18,"Azul")  // instanciar un objeto , objeto instanciado de una clase */
+console.log(woopa)
+
+const wooper=new Ajolonauta("wooper","java",10,"gris")
+console.log(wooper)
