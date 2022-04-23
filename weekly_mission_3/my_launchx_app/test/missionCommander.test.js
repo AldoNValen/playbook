5ddc2f53fc865d102998434187612538153d4c9d
AldoNValen <launchx06915@innovaccion.mx>
2022-04-22 22:38:28 -0500

    Se termina la primera practica

--- weekly_mission_3/my_launchx_app/test/missionCommander.test.js
+++ weekly_mission_3/my_launchx_app/test/missionCommander.test.js
@@ -2,11 +2,29 @@
  * Require es el estándar de CommonJS para exportar módulos en JS
  * Se necesita específicar la ruta del script de js a importar en este script
  * */
-const MissionCommander = require('./../app/missionCommander');
+/* //Primer ejemplo basico
+describe("Esto es una suite de pruebas", () => {
+  test('Caso de prueba 1', () => {
+    // se puede usar el codigo como se desee
+    //const result = 1 + 2
+    const result = 10
+    //se valida el resultado esperado
+    expect(result).toBe(10);
+  });
+}) */
+
+
+ const MissionCommander = require('./../app/missionCommander');
 
 describe("Unit Tests for Mission Commander Class", () => {
   test('1) Create a mission commander objet', () => {
     const myMissionCommander = new MissionCommander("Woopa")
+
+    // el primero falla porque se envia "Woopa" pero se espera "aldo"
+    //expect(myMissionCommander.name).toBe("aldo");
+
+    //el segundo resulta bien porque se envia "Woopa" y se espera recibir un "Woopa"
     expect(myMissionCommander.name).toBe("Woopa");
   });
-})
+}) 
+
