0f0be87306172a4247fe57e0b57270157e3ba00e
AldoNValen <launchx06915@innovaccion.mx>
2022-04-24 12:17:50 -0500

    agregando update para nombre de usuario

--- weekly_mission_3/twitter/app/services/UserService.js
+++ weekly_mission_3/twitter/app/services/UserService.js
@@ -4,12 +4,12 @@ class UserService {
   static create(id, username, name){
     return new User(id, username, name, "Sin bio")
   }
-
-  /* static getInfo(user){
+//nos regresa un objeto
+   static getInfo(user){
     return Object.values(user)
-  }
+  } 
 
-  static updateBio(user, bio){
+   static updateBio(user, bio){
     user.setBio = bio
   }
 
@@ -17,11 +17,11 @@ class UserService {
     user.setUsername = username
   }
 
-  static getAllUsernames(users){
+ /*  static getAllUsernames(users){
     const usersUsernames = users.map( user => user.username)
     return usersUsernames
-  }
- */
-}
+  } */
+ 
+} 
 
 module.exports = UserService
--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -2,31 +2,33 @@ const UserService = require('./../../app/services/UserService')
 
 describe("Tests for UserService", () => {
 
-  //PUNTO 4 , REUQERIMIENTO 1
+ /*  //PUNTO 4 , REUQERIMIENTO 1
  test("1. Create a new user using the UserService", () => {
     const user = UserService.create(1, "AldoVal", "Aldo")
     expect(user.username).toBe("AldoVal")
     expect(user.name).toBe("Aldo")
     expect(user.id).toBe(1)
     expect(user.bio).not.toBeUndefined()
-  }) 
+  })  */
 
- /*  test("2. Get all user data in a list", () => {
-    const user = UserService.create(1, "carlogilmar", "Carlo")
+  /* //PUNTO 4 , REUQERIMIENTO 2
+   test("2. Get all user data in a list", () => {
+    const user = UserService.create(1, "AldoVal", "Aldo")
+    //enviamos un objeto y nos lo regresa , lo mostramos en forma de lista
     const userInfoInList = UserService.getInfo(user)
     expect(userInfoInList[0]).toBe(1)
-    expect(userInfoInList[1]).toBe("carlogilmar")
-    expect(userInfoInList[2]).toBe("Carlo")
+    expect(userInfoInList[1]).toBe("AldoVal")
+    expect(userInfoInList[2]).toBe("Aldo")
     expect(userInfoInList[3]).toBe("Sin bio")
   })
+ */
+   test("3. Update username", () => {
+    const user = UserService.create(1, "AldoVal", "Aldo")
+    UserService.updateUserUsername(user, "Niko")
+    expect(user.username).toBe("Niko")
+  }) 
 
-  test("3. Update username", () => {
-    const user = UserService.create(1, "carlogilmar", "Carlo")
-    UserService.updateUserUsername(user, "carlog")
-    expect(user.username).toBe("carlog")
-  })
-
-  test("4. Given a list of users give me the list of usernames", () => {
+  /* test("4. Given a list of users give me the list of usernames", () => {
     const user1 = UserService.create(1, "carlogilmar1", "Carlo")
     const user2 = UserService.create(1, "carlogilmar2", "Carlo")
     const user3 = UserService.create(1, "carlogilmar3", "Carlo")
@@ -34,6 +36,6 @@ describe("Tests for UserService", () => {
     expect(usernames).toContain("carlogilmar1")
     expect(usernames).toContain("carlogilmar2")
     expect(usernames).toContain("carlogilmar3")
-  })  */
+  })   */
 
 })
