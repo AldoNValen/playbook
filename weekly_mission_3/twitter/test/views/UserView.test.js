53ecae76e6d8120019e466915e5c17b7be6c521c
AldoNValen <launchx06915@innovaccion.mx>
2022-04-24 13:05:54 -0500

    creando objeto user por medio d euna vista

--- weekly_mission_3/twitter/test/views/UserView.test.js
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -1,32 +1,40 @@
 const UserView = require('./../../app/views/UserView')
 
 describe("Tests for UserView", () => {
-
-  test("Return an error object when try to create a new user with an null payload", () => {
+  //punto 6, requerimiento 1
+   test("Return an error object when try to create a new user with an null payload", () => {
     const payload = null
     const result = UserView.createUser(payload)
     // https://jestjs.io/docs/using-matchers#strings
+
+    //aqui se verifica si el error contiene "playload no existe"
     expect(result.error).toMatch(/payload no existe/)
   })
+ 
 
-  test("Return an error object when try to create a new user with a payload with invalid properties", () => {
+  //punto 6, requerimiento 2
+    test("Return an error object when try to create a new user with a payload with invalid properties", () => {
     const payload = {username: null, name: 12, id: "id"}
     const result = UserView.createUser(payload)
-    expect(result.error).toMatch(/necesitan tener un valor válido/)
-  })
 
-  test("Return an error object when try to create a new user with a payload with missing properties", () => {
+    //aqui se verifica si el error contiene "necesitan tener un valor válido"
+    expect(result.error).toMatch(/necesitan tener un valor válido/)
+  })  
+
+   //punto 6, requerimiento 3
+   test("Return an error object when try to create a new user with a payload with missing properties", () => {
     const payload = {username: "Username"}
     const result = UserView.createUser(payload)
     expect(result.error).toMatch(/necesitan tener un valor válido/)
-  })
+  }) 
 
-  test("Create a user by a given valid payload", () => {
-    const payload = {username: "username", id: 1, name: "name"}
+   //punto 6, requerimiento 4
+   test("Create a user by a given valid payload", () => {
+    const payload = {username: "AldoVal", id: 1, name: "Aldo"}
     const result = UserView.createUser(payload)
-    expect(result.name).toBe("name")
-    expect(result.username).toBe("username")
+    expect(result.name).toBe("Aldo")
+    expect(result.username).toBe("AldoVal")
     expect(result.id).toBe(1)
-  })
+  }) 
 
 })
